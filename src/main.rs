c3d0533773691b1a5cf055a02231bac3301955e0
JuanGarcia0323 <juan.garcia@aridosoftware.com>
2024-10-02 16:24:16 -0300

    routes

--- src/main.rs
+++ src/main.rs
@@ -1,55 +1,51 @@
-use std::{
-    fs,
-    io::{prelude::*, BufReader},
-    net::{Shutdown, TcpListener, TcpStream},
-};
+pub mod requests;
+use requests::request_handler::{Connections, Methods, Router};
+use std::net::{TcpListener, TcpStream};
 
-fn handle_connection(mut stream: TcpStream) {
-    let buf_reader = BufReader::new(&mut stream);
-    let response =
-        fs::read("C:\\Users\\juane\\Desktop\\Rust\\basic-http-server\\http-server\\response.txt")
-            .unwrap();
-    let http_request: Vec<String> = buf_reader
-        .lines()
-        .map(|result| result.unwrap())
-        .take_while(|line| !line.is_empty())
-        .collect();
-    let request = http_request.join("\n");
-    stream.write(&response).unwrap();
-    println!("Request: {request}");
+fn handle_connection(stream: TcpStream) {
+    let mut router = Router::new();
+    let connection = Connections::new(stream);
 
-    stream.shutdown(Shutdown::Both).unwrap();
+    let method = connection.get_method();
+    let route = connection.get_route();
+
+    fn test_get() {
+        println!("Test");
+    }
+    router.create("/otra-ruta/", Methods::GET, test_get);
+    router.handle_connection(route, method);
+    // println!("Este es el request:\n {}", connection.get_request());
+    // let method = connection.get_method().unwrap();
+    // let route = connection.get_route().unwrap();
+
+    // match route {
+    //     Some(r) => println!("{r}"),
+    //     _ => (),
+    // }
+
+    // match method {
+    //     Some(Methods::GET) => {
+    //         println!("Vamos chabon que era un GET!")
+    //     }
+    //     _ => (),
+    // }
+
+    // let test_router = || {
+    //     println!("Testing");
+    //     connection.write_response("Esto viene del response");
+    // };
+
+    // router.create(route, &method, &test_router);
 }
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:3000").unwrap();
     for stream in listener.incoming() {
         match stream {
-            Ok(stream) => {
-                handle_connection(stream);
-            }
+            Ok(stream) => handle_connection(stream),
             Err(_) => {
                 println!("No anda");
             }
         }
     }
-    // match listener.accept() {
-    //     Ok((mut socket, _)) => handle_client(&mut socket),
-    // let mut buff = String::new();
-    // match socket.read_to_string(&mut buff) {
-    //     Ok(_) => {
-    //         println!("{buff}");
-    //     }
-    //     Err(_) => socket
-    //         .shutdown(Shutdown::Both)
-    //         .expect("Something went wrong"),
-    // };
-    // if buff.contains("GET") {
-    //     println!("Es un GET")
-    // }
-    // Err(_) => (),
-    // }
-
-    // for stream in listener.incoming() { handle_client(stream.unwrap());
-    // }
 }
---
+++ src/requests/request_handler.rs
@@ -0,0 +1,137 @@
+use std::{
+    collections::HashMap,
+    io::{prelude::*, BufReader},
+    net::{Shutdown, TcpStream},
+    rc::Rc,
+};
+
+#[derive(Debug)]
+pub enum Methods {
+    GET,
+    POST,
+    PUT,
+    DELETE,
+    PATCH,
+}
+
+static SUCCES_RESPONSE_FILE: &'static str = include_str!("./response.txt");
+
+#[derive(Debug)]
+pub struct Connections {
+    stream: TcpStream,
+    request: Vec<String>,
+    method: Option<Methods>,
+    route: Option<String>,
+}
+impl Connections {
+    pub fn new(mut stream: TcpStream) -> Self {
+        let buf_reader = BufReader::new(&mut stream);
+        let http_request: Vec<String> = buf_reader
+            .lines()
+            .map(|result| result.unwrap())
+            .take_while(|line| !line.is_empty())
+            .collect();
+
+        let first_line: Vec<&str> = match http_request.get(0) {
+            Some(line) => line.split(" ").collect(),
+            None => vec![],
+        };
+
+        let method = match first_line.get(0) {
+            Some(m) => *m,
+            None => "",
+        };
+
+        let route = match first_line.get(1) {
+            Some(m) => Some(String::from(*m)),
+            None => None,
+        };
+
+        match stream.write(b"HTTP/1.1 200 OK") {
+            Ok(_) => {
+                println!("Response was writen")
+            }
+            _ => stream
+                .shutdown(Shutdown::Both)
+                .expect("Somenthing went wrong"),
+        };
+
+        Connections {
+            stream,
+            method: match method {
+                "GET" => Some(Methods::GET),
+                "POST" => Some(Methods::POST),
+                "PUT" => Some(Methods::PUT),
+                "DELETE" => Some(Methods::DELETE),
+                "PATCH" => Some(Methods::PATCH),
+                _ => None,
+            },
+            request: http_request,
+            route,
+        }
+    }
+
+    pub fn write_response(&mut self, message: &str) {
+        let response = SUCCES_RESPONSE_FILE
+            .replace("{{len}}", &message.len().to_string())
+            .replace("{{message}}", &message);
+
+        self.stream.write(&response.as_bytes()).unwrap();
+    }
+
+    pub fn get_request(&self) -> String {
+        self.request.join("\n")
+    }
+
+    pub fn get_route(&self) -> &str {
+        match &self.route {
+            Some(r) => &r,
+            None => "",
+        }
+    }
+
+    pub fn get_method(&self) -> &Methods {
+        match &self.method {
+            Some(m) => &m,
+            None => &Methods::GET,
+        }
+    }
+}
+
+pub struct Router<'a> {
+    put: HashMap<&'a str, fn()>,
+    post: HashMap<&'a str, fn()>,
+    delete: HashMap<&'a str, fn()>,
+    patch: HashMap<&'a str, fn()>,
+    get: HashMap<&'a str, fn()>,
+}
+impl<'a> Router<'a> {
+    pub fn new() -> Router<'a> {
+        Router {
+            put: HashMap::new(),
+            post: HashMap::new(),
+            delete: HashMap::new(),
+            patch: HashMap::new(),
+            get: HashMap::new(),
+        }
+    }
+    pub fn create(&mut self, name: &'a str, method: Methods, action: fn()) {
+        match method {
+            Methods::GET => self.get.insert(name, action),
+            Methods::PUT => self.put.insert(name, action),
+            Methods::POST => self.post.insert(name, action),
+            Methods::PATCH => self.patch.insert(name, action),
+            Methods::DELETE => self.delete.insert(name, action),
+        };
+    }
+    pub fn handle_connection(&self, name: &'a str, method: &'a Methods) {
+        let action = *match method {
+            Methods::GET => self.get.get(&name).unwrap(),
+            Methods::POST => self.post.get(&name).unwrap(),
+            Methods::PUT => self.put.get(&name).unwrap(),
+            Methods::PATCH => self.patch.get(&name).unwrap(),
+            Methods::DELETE => self.delete.get(&name).unwrap(),
+        };
+        action();
+    }
+}
